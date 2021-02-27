1ab5ac29ab0254c694a099f1cde565d9e18557cd
neeleshmehto <40487138+neeleshmehto@users.noreply.github.com>
2021-02-27 22:46:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "0.12"
 }
 provider "aws" {
-	access_key = "${var.aws_access_key}"
-        secret_key = "${var.aws_secret_key}"
+	access_key = ""    
+        secret_key = ""
 	region = "${var.aws_region}"
 }
 
